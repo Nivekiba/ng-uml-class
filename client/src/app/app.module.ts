ea0009387cf57ffc1596dbceab533dbb51aa0b54
paola2020 <ngwepaola@gmail.com>
2019-01-08 16:39:56 +0100

    src

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ROUTING } from './app.routing';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { RootComponent } from './root/root.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {MaterialModule} from './material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AppRouters} from './app.routes';
+import {DataService} from './data/data.service';
+import {AuthService} from './auth.service';
+import {PostDialogComponent} from './post-dialog/post-dialog.component';
+import {FormsModule} from '@angular/forms';
+import { RegistrationComponentComponent } from './registration-component/registration-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
 @NgModule({
   declarations: [
-  	RootComponent,
-    LoginComponent,
-    HomeComponent,
+    AppComponent,
+    WelcomeComponent,
+    DashboardComponent,
+    PostDialogComponent,
+	RegistrationComponentComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
-    ROUTING,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AppRouters,
     FormsModule,
-    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [RootComponent]
+  providers: [DataService, AuthService],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    PostDialogComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
